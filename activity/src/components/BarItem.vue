157fe792f5c4f6d7555abe713c6e93f429b38a35
Wenjie Xu <563030073@qq.com>
2018-12-03 01:24:54 +0800

    add BarItem component

---
+++ activity/src/components/BarItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="BarItem">
+        <slot name="Icon"></slot>
+        <slot name="Img"></slot>  
+        <br/>         
+        <slot name="Label"></slot>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'BarItem'
+}
+</script>
+<style scoped>
+
+</style>
+
+
