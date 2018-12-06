9491bdf37562c08c5348b4df4fa8c5862c4d45aa
CR7WD3 <3501491633@qq.com>
2018-12-06 20:04:25 +0800

    添加main

--- activity/src/views/main.vue
+++ activity/src/views/main.vue
@@ -1,9 +1,58 @@
 <template>
-    
+  <div id="main">
+    <div class='hn'>
+      <headnav></headnav>
+    </div>
+    <div class='ca'>
+      <calender></calender>
+    </div>
+    <div class='pj'>
+      <pros></pros>
+    </div>
+    <div class='bn'>
+      <bottomnav></bottomnav>
+    </div>
+  </div>
 </template>
 
 <script>
+import pros from '../components/scrollCard/pros.vue'
+import calender from '../components/calender/'
+import bottomnav from '../components/bottomNav/BottomNav.vue'
+import headnav from '../components/headNav/HeadNav.vue'
 export default {
-    
+  components: {
+    pros,
+    calender,
+    bottomnav,
+    headnav
+  }
 }
 </script>
+  
+<style scoped>
+  #main{
+      margin-top: 55px;
+  }
+  /* .hn{
+    position: fixed;
+    top:0px;
+    height: 55px;
+  }
+  .ca{
+    position: fixed;
+    top:55px;
+    width: 360px;
+  }
+  .pj{
+    position: fixed;
+    bottom:50px;
+    height: 140px;
+    width: 100%;
+    background-color: #fff;
+  }
+  .bn{
+    position: fixed;
+    bottom: 0; 
+  } */
+</style>
