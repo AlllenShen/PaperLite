8f209cdb97138253545acc19e6d7531b2e70a18e
AllenShen <932142511@qq.com>
2018-12-09 18:28:31 +0800

    不使用router

--- activity/src/router.js
+++ activity/src/router.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import mine from './components/scrollCard/mine.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/mine',
-      component: mine
-    }
+    
   ]
 })
