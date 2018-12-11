f6d47d5b37dbf5117dbacf58773196c193771270
Wenjie Xu <563030073@qq.com>
2018-12-11 15:35:38 +0800

    add files

---
+++ activity/src/components/bottomNav/BottomNav.vue
@@ -0,0 +1,25 @@
+<template>
+    <BottomBar v-bind:items="items"></BottomBar>
+</template>
+
+<script>
+import BottomBar from './BottomBar.vue'
+export default {
+    data () {
+        return{
+            items:[
+                {id:1,label:"首页",icon_name:"md-calendar",icon_color:"white",icon_size:"20"},
+                {id:2,label:"浏览",icon_name:"md-list",icon_color:"white", icon_size:"20"},
+                {id:3,label:"我的",icon_name:"md-person",icon_color:"white",icon_size:"20"}
+            ]
+        }
+    },
+    components:{
+        'BottomBar': BottomBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
