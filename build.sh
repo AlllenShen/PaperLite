b225716cc2995ac5d1970324e9305080287466a6
Allen Shen <932142511@qq.com>
2018-12-21 00:18:46 +0800

    fix build无法执行

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo -----------------
 echo run build.sh
-cd Activity
+echo -----------------
+cd /root/projects/PaperLite/Activity
 npm install
 npm run build
+echo -----------------
 echo build finished
 echo -----------------
