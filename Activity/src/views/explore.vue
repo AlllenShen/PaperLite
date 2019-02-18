558e8268c2d8119463daa5aad404111da1e4390b
AllenShen <932142511@qq.com>
2019-02-18 22:57:16 +0800

    用vuex管理样式变量

--- Activity/src/views/explore.vue
+++ Activity/src/views/explore.vue
@@ -1,19 +1,27 @@
 <template>
     <div id="explore">
         <mainHead title="探索"></mainHead>
-        <actList></actList>
+        <actList :style="{ marginTop: heightOfHeader }"></actList>
         <mainBottom current="探索"></mainBottom>
     </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+
 import { mainBottom, mainHead } from '../components/mainNav/'
 import actList from '../components/activityList/'
+
 export default {
   components: {
     mainBottom,
     mainHead,
     actList,
+  },
+  computed: {
+    ...mapState({
+      heightOfHeader: state => state.style.heightOfHeader + 'px'
+    })
   }
 }
 </script>
