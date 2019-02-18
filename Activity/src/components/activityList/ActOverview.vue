f0a1c2de2db91ca8ae2617927a6473908c9bb2fa
Wenjie Xu <563030073@qq.com>
2019-02-18 14:08:46 +0800

    api call

--- Activity/src/components/activityList/ActOverview.vue
+++ Activity/src/components/activityList/ActOverview.vue
@@ -1,14 +1,18 @@
 <template>   
     <div id="ActOverview">
-        <div class="actlist" v-bind:key="act.actkey" v-for="(act,index) in activities">
-            <div class="blank"></div>
+        <!--<div class="actlist" v-bind:key="act.actkey" v-for="(act,index) in activities">-->
+        <div class="actlist" v-for="(act,index) in activities">
             <div class="frame">
                 <div class="typecolor" :style="{'background-color':bgcolor(act)}"></div>
-                <h3>{{act.actname}}</h3>
+                <!--<h3>{{act.actname}}</h3>
                 <p>时间：{{act.acttime}}</p>
                 <p>地点：{{act.actplace}}</p>
                 <p>参与对象：{{act.actmember}}</p>
-                <p>说明：{{act.actnote}}</p>  
+                <p>说明：{{act.actnote}}</p>-->
+                <h3>{{act.title}}</h3>
+                <p>报名时间：{{act.apply_begin_at}}~{{act.apply_end_at}}</p>
+                <p>参与对象：{{act.accepted_apply}}</p>
+                <p>组织者：{{act.organization}}</p>  
                 <button id="signup">报&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名</button> 
                 <button id="detail">详&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;情</button>           
             </div>           
@@ -21,7 +25,7 @@
 export default {
     props:['activities'],
     methods:{
-        bgcolor:function(act){
+        /*bgcolor:function(act){
             if(act.acttype=='竞赛活动'){
                 return '#ffba5a'
             }
@@ -31,19 +35,28 @@ export default {
             if(act.acttype=='志愿服务'){
                 return '#ff7657'
             }
+        }*/
+        bgcolor:function(act){
+            if(act.type=='竞赛活动'){
+                return '#ffba5a'
+            }
+            if(act.type=='报告讲座'){
+                return '#665c84'
+            }
+            if(act.type=='志愿服务'){
+                return '#ff7657'
+            }
         }
     }    
 }
 </script>
 
 <style scoped>
-.blank{
-    width: 100%;
-    height: 12px;
-}
 .frame{
     position: relative;
     text-align: center;
+    margin-top: 12px;
+    margin-bottom: 12px;
     margin-left: 2%;
     width: 96%;
     height: 150px;
@@ -85,6 +98,10 @@ export default {
     border-top-right-radius: 5px;
     border-bottom-right-radius: 5px;
     background-color: #ff7657;
+}
+.blank{
+    width: 100%;
+    height: 55px;
 }
 </style>
 
