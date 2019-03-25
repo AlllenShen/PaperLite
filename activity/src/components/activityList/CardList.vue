bb4d41f773ad2bc6c094bc22dcf0aafe773d6111
Wenjie Xu <563030073@qq.com>
2019-03-25 23:50:04 +0800

    modify main actcard cardlist search

--- activity/src/components/activityList/CardList.vue
+++ activity/src/components/activityList/CardList.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="CardList">
-    <ActCard :activities="activities"></ActCard>
+    <ActCard :activities="currentActs"></ActCard>
   </div>
 </template>
 
@@ -8,44 +8,26 @@
 import ActCard from './ActCard.vue'
 import { mapState, mapGetters } from 'vuex'
 export default {
-    /*data () {
-        return{
-            activity:'http://api.wyyxhlxy.com/activity/search',
-            activities:[]
-        }
-    },*/
     computed: {
         ...mapState({
-            activities:state=>state.activity.activities
+            currentActs:state=>state.activity.currentActs,
+            bgjzpage:state=>state.activity.bgjzpage,
+            jshdpage:state=>state.activity.jshdpage,
+            zyfupage:state=>state.activity.zyfupage,
+            pagemap:state=>state.activity.pagemap,
         }),
         ...mapGetters([
-            'activityAPI'
+            'activitySearch'
         ]),
     },
     components:{
         'ActCard':ActCard
     },
-    mounted:function(){
-        this.getacts()
-        //this.$store.commit('getacts')
-    },
-    methods:{
-        getacts:function(){
-            this.$http.post(this.activityAPI).then((response) => {
-                let data = response.data.result
-                console.log(this.activityAPI)
-                console.log(response)
-                //$store.state.activities=response.data.result
-                this.$store.commit('getacts',data)
-                //console.log($store.activity.activities);
-            },(response) => {
-                console.log(response);
-            })
-        }
-    }
 }
 </script>
 
 <style scoped>
-
+#CardList{
+    background-color: #F6F6F5;
+}
 </style>
