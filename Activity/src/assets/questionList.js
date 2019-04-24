d334be372e5370636d0b2d2b5b92d3be35c7a115
CR7WD3 <3501491633@qq.com>
2019-04-24 13:51:19 +0800

    评价优化

--- Activity/src/assets/questionList.js
+++ Activity/src/assets/questionList.js
@@ -2,234 +2,289 @@ const questionList = [
     [
         {
             quskey: 1,
-            name: '规则有缺陷'
+            name: '规则有缺陷',
+            flag: false,
         },
         {
             quskey: 2,
-            name: '工作人员态度恶劣'
+            name: '工作人员态度恶劣',
+            flag: false,
         },
         {
             quskey: 3,
-            name: '奖励与宣传时不符'
+            name: '奖励与宣传时不符',
+            flag: false,
         },
         {
             quskey: 4,
-            name: '流程混乱'
+            name: '流程混乱',
+            flag: false,
         },
         {
             quskey: 5,
-            name: '工作人员未能提供必要帮助'
+            name: '工作人员未能提供必要帮助',
+            flag: false,
         },
         {
             quskey: 6,
-            name: '场地布置不美观'
+            name: '场地布置不美观',
+            flag: false,
         },
         {
             quskey: 7,
-            name: '未能准时开始'
+            name: '未能准时开始',
+            flag: false,
         },
         {
             quskey: 8,
-            name: '材料准备不充分'
+            name: '材料准备不充分',
+            flag: false,
         },
         {
             quskey: 9,
-            name: '活动时间过长'
+            name: '活动时间过长',
+            flag: false,
         },
         {
             quskey: 10,
-            name: '其他问题'
+            name: '其他问题',
+            flag: false,
         }
     ],
     [
         {
             quskey: 11,
-            name: '工作人员态度恶劣'
+            name: '工作人员态度恶劣',
+            flag: false,
         },
         {
             quskey: 12,
-            name: '奖励与宣传时不符'
+            name: '奖励与宣传时不符',
+            flag: false,
         },
         {
             quskey: 13,
-            name: '流程混乱'
+            name: '流程混乱',
+            flag: false,
         },
         {
             quskey: 14,
-            name: '工作人员未能提供必要帮助'
+            name: '工作人员未能提供必要帮助',
+            flag: false,
         },
         {
             quskey: 15,
-            name: '场地布置不美观'
+            name: '场地布置不美观',
+            flag: false,
         },
         {
             quskey: 16,
-            name: '未能准时开始'
+            name: '未能准时开始',
+            flag: false,
         },
         {
             quskey: 17,
-            name: '材料准备不充分'
+            name: '材料准备不充分',
+            flag: false,
         },
         {
             quskey: 18,
-            name: '活动时间过长'
+            name: '活动时间过长',
+            flag: false,
         },
         {
             quskey: 19,
-            name: '规则有缺陷'
+            name: '规则有缺陷',
+            flag: false,
         },
         {
             quskey: 20,
-            name: '其他问题'
+            name: '其他问题',
+            flag: false,
         }
     ],
     [
         {
             quskey: 21,
-            name: '奖励与宣传时不符'
+            name: '奖励与宣传时不符',
+            flag: false,
         },
         {
             quskey: 22,
-            name: '流程混乱'
+            name: '流程混乱',
+            flag: false,
         },
         {
             quskey: 23,
-            name: '工作人员未能提供必要帮助'
+            name: '工作人员未能提供必要帮助',
+            flag: false,
         },
         {
             quskey: 24,
-            name: '场地布置不美观'
+            name: '场地布置不美观',
+            flag: false,
         },
         {
             quskey: 25,
-            name: '未能准时开始'
+            name: '未能准时开始',
+            flag: false,
         },
         {
             quskey: 26,
-            name: '材料准备不充分'
+            name: '材料准备不充分',
+            flag: false,
         },
         {
             quskey: 27,
-            name: '活动时间过长'
+            name: '活动时间过长',
+            flag: false,
         },
         {
             quskey: 28,
-            name: '规则有缺陷'
+            name: '规则有缺陷',
+            flag: false,
         },
         {
             quskey: 29,
-            name: '工作人员态度恶劣'
+            name: '工作人员态度恶劣',
+            flag: false,
         },
         {
             quskey: 30,
-            name: '其他问题'
+            name: '其他问题',
+            flag: false,
         }
     ],
     [
         {
             quskey: 31,
-            name: '流程混乱'
+            name: '流程混乱',
+            flag: false,
         },
         {
             quskey: 32,
-            name: '工作人员未能提供必要帮助'
+            name: '工作人员未能提供必要帮助',
+            flag: false,
         },
         {
             quskey: 33,
-            name: '场地布置不美观'
+            name: '场地布置不美观',
+            flag: false,
         },
         {
             quskey: 34,
-            name: '未能准时开始'
+            name: '未能准时开始',
+            flag: false,
         },
         {
             quskey: 35,
-            name: '材料准备不充分'
+            name: '材料准备不充分',
+            flag: false,
         },
         {
             quskey: 36,
-            name: '活动时间过长'
+            name: '活动时间过长',
+            flag: false,
         },
         {
             quskey: 37,
-            name: '规则有缺陷'
+            name: '规则有缺陷',
+            flag: false,
         },
         {
             quskey: 38,
-            name: '工作人员态度恶劣'
+            name: '工作人员态度恶劣',
+            flag: false,
         },
         {
             quskey: 39,
-            name: '奖励与宣传时不符'
+            name: '奖励与宣传时不符',
+            flag: false,
         },
         {
             quskey: 40,
-            name: '其他问题'
+            name: '其他问题',
+            flag: false,
         }
     ],
     [
 
         {
             quskey: 41,
-            name: '工作人员未能提供必要帮助'
+            name: '工作人员未能提供必要帮助',
+            flag: false,
         },
         {
             quskey: 42,
-            name: '场地布置不美观'
+            name: '场地布置不美观',
+            flag: false,
         },
         {
             quskey: 43,
-            name: '未能准时开始'
+            name: '未能准时开始',
+            flag: false,
         },
         {
             quskey: 44,
-            name: '材料准备不充分'
+            name: '材料准备不充分',
+            flag: false,
         },
         {
             quskey: 45,
-            name: '活动时间过长'
+            name: '活动时间过长',
+            flag: false,
         },
         {
             quskey: 46,
-            name: '规则有缺陷'
+            name: '规则有缺陷',
+            flag: false,
         },
         {
             quskey: 47,
-            name: '工作人员态度恶劣'
+            name: '工作人员态度恶劣',
+            flag: false,
         },
         {
             quskey: 48,
-            name: '奖励与宣传时不符'
+            name: '奖励与宣传时不符',
+            flag: false,
         },
         {
             quskey: 49,
-            name: '流程混乱'
+            name: '流程混乱',
+            flag: false,
         },
         {
             quskey: 50,
-            name: '其他问题'
+            name: '其他问题',
+            flag: false,
         }
     ],
     [
         {
             quskey: 51,
-            name: '流程有序'
+            name: '流程有序',
+            flag: false,
         },
         {
             quskey: 52,
-            name: '干货满满'
+            name: '干货满满',
+            flag: false,
         },
         {
             quskey: 53,
-            name: '工作人员态度好服务棒'
+            name: '工作人员态度好服务棒',
+            flag: false,
         },
         {
             quskey: 54,
-            name: '活动氛围好，充分调动参与者的积极性'
+            name: '活动氛围好',
+            flag: false,
         },
         {
             quskey: 55,
-            name: '其他问题'
+            name: '其他问题',
+            flag: false,
         }
     ]
 ]
